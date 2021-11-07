82b857e870d82680546a4e34c51ec67825c38f44
HJ Lee <umtt0820@gmail.com>
2021-11-07 22:07:31 +0900

    이벤트 등록

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,20 +1,34 @@
-import World from "./World";
 import styles from "./Hello.module.css";
 
 export default function Hello() {
+  function showName() {
+    console.log("Mike");
+  }
+  function showAge(age){
+    console.log(age);
+  }
+  // function showText(e){
+  //   console.log(e.target.value);
+  // }
+
   return (
     <div>
-      <h1
-        style={{
-          color: "#f00",
-          borderRight: "12px solid #000",
-          marginBottom: "30px",
-          opacity: 1,
+      <h1>Hello</h1>
+      <button onClick={showName}>Show name</button>
+      <button
+        onClick={() => {
+          showAge(30);
         }}
       >
-        Hello
-      </h1>
-      <div className={styles.box}>Hello</div>
+        Show age
+      </button>
+      {/* <input type="text" onChange={showText} /> */}
+      <input
+        type="text"
+        onChange={ e => {
+          console.log(e.target.value);
+        }}
+      />
     </div>
   );
 }
