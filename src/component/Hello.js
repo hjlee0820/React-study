aa6907800e22dbbde9cebfbbaf659e69e0b11603
HJ Lee <umtt0820@gmail.com>
2021-11-07 22:41:59 +0900

    props로 받은 값을 state의 초기값으로 복사하여 컴포넌트 내부에서 사용하기

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,17 +1,20 @@
 import { useState } from "react";
 
-export default function Hello() {
+export default function Hello(props) {
+  console.log(props);
   const [name, setName] = useState("Mike");
+  const [age, setAge] = useState(props.age);
 
-  function changeName() {
+  function btnClick() {
     setName(name === "Mike" ? "Jane" : "Mike");
+    setAge(age + 1);
   }
 
   return (
     <div>
       <h1>State</h1>
-      <h2>{name}</h2>
-      <button onClick={changeName}>Change</button>
+      <h2>{name}({age})</h2>
+      <button onClick={btnClick}>Change</button>
     </div>
   );
 }
