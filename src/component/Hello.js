0323872616f7d65b9c62b4ca53ffc469c06357e3
HJ Lee <umtt0820@gmail.com>
2021-11-08 12:54:10 +0900

    props2

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,20 +1,43 @@
 import { useState } from "react";
+import UserName from "./UserName";
 
-export default function Hello(props) {
-  console.log(props);
+// export default function Hello(props) {
+//   console.log(props);
+//   const [name, setName] = useState("Mike");
+//   const [age, setAge] = useState(props.age);
+
+//   function btnClick() {
+//     setName(name === "Mike" ? "Jane" : "Mike");
+//     setAge(age + 1);
+//   }
+
+//   return (
+//     <div>
+//       <h1>State</h1>
+//       <h2>{name}({age})</h2>
+//       <button onClick={btnClick}>Change</button>
+//     </div>
+//   );
+// }
+
+export default function Hello({age}) {
   const [name, setName] = useState("Mike");
-  const [age, setAge] = useState(props.age);
+  const msg = age > 19 ? '성인입니다.' : '미성년자입니다.';
 
   function btnClick() {
     setName(name === "Mike" ? "Jane" : "Mike");
-    setAge(age + 1);
   }
 
   return (
     <div>
       <h1>State</h1>
-      <h2>{name}({age})</h2>
-      <button onClick={btnClick}>Change</button>
+      <h2 id="name">
+        {name}({age}) : {msg}
+      </h2>
+      <UserName name={name}/>
+      <button
+        onClick={btnClick}
+      >Change</button>
     </div>
   );
 }
