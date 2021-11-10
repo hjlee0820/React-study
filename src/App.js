af589ed365a3f830bd9f943c58ac69a528db31fe
HJ Lee <umtt0820@gmail.com>
2021-11-10 20:45:09 +0900

    참고중인 영상에서 사용중인 버전과 최신버전과의 차이 때문에 진행 보류
    추가 공부 필요

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import DayList from "./component/DayList";
 import Day from "./component/Day";
 import Header from "./component/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <DayList />
-      <Day />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route exact path="/">
+            <DayList />
+          </Route>
+          <Route pat="/day">
+            <Day />
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/component/DayList.js
+++ src/component/DayList.js
@@ -4,10 +4,12 @@ import Day from './Day';
 export default function DayList(){
   console.log(dummy);
   return(
-    <ul className="list_day">
-      {dummy.days.map(day => (
-        <li key={day.id}>Day {day.day}</li>
-      ))}
-    </ul>
+    <>
+      <ul className="list_day">
+        {dummy.days.map(day => (
+          <li key={day.id}>Day {day.day}</li>
+        ))}
+      </ul>
+    </>
   );
 }
